e40f746b57f68933a51ab25b8687602c3e8027b3
Vic <vitrexesp@gmail.com>
2022-09-24 13:15:09 +0200

    Version 1.0

--- plant_identifier.py
+++ plant_identifier.py
@@ -6,12 +6,15 @@ import os
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import messagebox
-from serpapi import GoogleSearch
+# from serpapi import GoogleSearch
 from PIL import ImageTk, Image
 from threading import Thread
+from fpdf import FPDF
+from datetime import datetime
+import wikipedia
 
 load_dotenv()
-
+wikipedia.set_lang('es')
 
 class PlantClassificatorUI:
 
@@ -30,6 +33,8 @@ class PlantClassificatorUI:
         self.result_label = Label(text="", font=("Helvetica", 12, "bold"), background='#17ED4F')
         self.result_label.grid(row=2, column=0, columnspan=2)
         
+        self.cannonical_Name = None
+        
         self.label1 = Label()
 
         self.window.mainloop()
@@ -44,12 +49,16 @@ class PlantClassificatorUI:
         filenames = fd.askopenfilenames(
             title="Select images", filetypes=filetypes, initialdir="/")
         plant_images = list(filenames)
-            
-        if len(plant_images) > 2:
-            messagebox.showinfo(
-                title="Atención!", message="Debes seleccionar un máximo de dos imágenes!")
+        
+        if plant_images:
+            if len(plant_images) > 2:
+                messagebox.showinfo(
+                    title="Atención!", message="Debes seleccionar un máximo de dos imágenes!")
+            else:
+                return plant_images
         else:
-            return plant_images
+            self.select_imgs.config(state=ACTIVE, text="Choose plant")
+            
 
     def detect_plant(self):
         plants_imgs = self.select_plant_imgs()
@@ -101,16 +110,18 @@ class PlantClassificatorUI:
                 bestMatch = json_result.get('bestMatch')
                 result1_accuracy = json_result.get('results')[0].get('score')
                 
-                self.result_label.config(text=f"Best match is '{bestMatch}' with {result1_accuracy} score.")
-                images = self.serpapi_get_google_images(bestMatch.split(" ")[0])
+                self.result_label.config(text=f"Best match is '{bestMatch}' with {result1_accuracy * 100}% score.")
+                # images = self.serpapi_get_google_images(bestMatch.split(" ")[0])
                 remainingRequests = json_result.get(
                     'remainingIdentificationRequests')
 
                 print(
                     f"Best Match: {bestMatch}\nPuedes identificar {remainingRequests} plantas más!")
-                print(f"Similar images: {images}")
-                print("Showing first 5 results:")
-
+                # print(f"Similar images: {images}")
+                
+                pdf = FPDF('P', 'mm', 'Letter')
+                
+                num = 120
                 for n, res in enumerate(json_result.get('results')[0:5], start=1):
                     gbifCode = res.get('gbif').get('id')
                     response2 = requests.get(
@@ -127,61 +138,104 @@ class PlantClassificatorUI:
                     kingdom = json_result_2.get('kingdom')
                     family = json_result_2.get('family')
                     species = json_result_2.get('species')
-                    cannonicalName = json_result_2.get('canonicalName')
+                    self.cannonical_Name = json_result_2.get('canonicalName')
                     accuracyScore = round(res.get('score') * 100, 2)
                     commonNames = res.get('species').get('commonNames')
                     scientificName = res.get('species').get('scientificName')
                     scientificAuthor = res.get('species').get(
                         'scientificNameAuthorship')
-                    print(f"\nResult {n}:")
-                    print(f"	- Gbif code: {gbifCode}")
-                    print(f"	- Precission score: {res.get('score')} ({accuracyScore}%)")
-                    print(f"	- Common names: {commonNames}")
-                    print(f"	- Cannonical name: {cannonicalName}")
-                    print(f"	- Scientific name: {scientificName}")
-                    print(f"	- Species: {species}")
-                    print(f"	- Family: {family}")
-                    print(f"	- Kingdom: {kingdom}")
-                    print(f"	- Author: {scientificAuthor}")
-                    print(f"	- Similar images: {similar_image}")
+                    
+                    if n == 1:
+                        similar_images = self.get_wiki_images(self.cannonical_Name)
+                        print(f"Similar images: {similar_images}")
+                        self.get_wiki_info(self.cannonical_Name)
+                    
+                    pdf.add_page()
 
-    def serpapi_get_google_images(self, query):
-        image_results = []
+                    pdf.set_font('helvetica', '', 14)
 
-        # search query parameters
-        params = {
-            # search engine. Google, Bing, Yahoo, Naver, Baidu...
-            "engine": "google",
-            "q": query,                       # search query
-            "tbm": "isch",                    # image results
-            "num": "20",                     # number of images per page
-            # page number: 0 -> first page, 1 -> second...
-            "ijn": 0,
-            # your serpapi api key
-            "api_key": os.environ["API_KEY2"]
-            # other query parameters: hl (lang), gl (country), etc
-        }
+                    pdf.cell(num, 10, f'Result {n}:', ln=True)
+                    pdf.cell(num, 10, f'- Gbif code: {gbifCode}', ln=True)
+                    pdf.cell(num, 10, f'- Precission score: {res.get("score")} ({accuracyScore}%)', ln=True)
+                    pdf.cell(num, 10, f'- Common names: {commonNames}', ln=True)
+                    pdf.cell(num, 10, f'- Cannonical name: {self.cannonical_Name}', ln=True)
+                    pdf.cell(num, 10, f'- Scientific name: {scientificName}', ln=True)
+                    pdf.cell(num, 10, f'- Species: {species}', ln=True)
+                    pdf.cell(num, 10, f'- Family: {family}', ln=True)
+                    pdf.cell(num, 10, f'- Kingdom: {kingdom}', ln=True)
+                    pdf.cell(num, 10, f'- Author: {scientificAuthor}', ln=True)
+                    
+                    num -= 20
 
-        # where data extraction happens
-        search = GoogleSearch(params)
-
-        image_results = []
-        results = search.get_dict()
-        # checks for "Google hasn't returned any results for this query."
-        try:
-            if "error" not in results:
-                for image in results["images_results"]:
-                    if image["original"] not in image_results:
-                        image_results.append(image["original"])
-        except KeyError:
-            print("No se han encontrado imágenes similares...")
-        else:
-            return image_results[0:5]
+                yn = messagebox.askyesno(title="Download PDF?", message="Do you want to download a PDF with more results?")
+                if yn:
+                    pdf.output(f'Plant_{gbifCode}_{bestMatch}_{datetime.now().strftime("%Y-%m-%d")}.pdf')
+                    messagebox.showinfo(title="Info", message="Pdf report created succesfully!")
+                else:
+                    pass
+                self.select_imgs.config(state=ACTIVE, text="Choose plant")
+                
+    def get_wiki_images(self, plant_name):
         
-    def threading(self): 
+        images = wikipedia.page(plant_name).images
+        
+        final_images = []
+        for img in images:
+            if img.endswith('.jpg') or img.endswith('.jpeg'):
+                final_images.append(img)  
+            else:
+                pass
+        
+        return final_images
     
-        t1=Thread(target=self.detect_plant) 
-        t1.start() 
+    def get_wiki_info(self, plant_name):
+        summary = wikipedia.summary(plant_name)
+        title = wikipedia.page(plant_name).title
+        
+        messagebox.showinfo(title=f"Wiki '{title}' info", message=summary)
+        
+                  
+    # def serpapi_get_google_images(self, query):
+    #     image_results = []
+
+    #     # search query parameters
+    #     params = {
+    #         # search engine. Google, Bing, Yahoo, Naver, Baidu...
+    #         "engine": "google",
+    #         "q": query,                       # search query
+    #         "tbm": "isch",                    # image results
+    #         "num": "20",                     # number of images per page
+    #         # page number: 0 -> first page, 1 -> second...
+    #         "ijn": 0,
+    #         # your serpapi api key
+    #         "api_key": os.environ["API_KEY2"]
+    #         # other query parameters: hl (lang), gl (country), etc
+    #     }
+
+    #     # where data extraction happens
+    #     search = GoogleSearch(params)
+
+    #     image_results = []
+    #     results = search.get_dict()
+    #     # checks for "Google hasn't returned any results for this query."
+    #     try:
+    #         if "error" not in results:
+    #             for image in results["images_results"]:
+    #                 if image["original"] not in image_results:
+    #                     image_results.append(image["original"])
+    #     except KeyError:
+    #         print("No se han encontrado imágenes similares...")
+    #     else:
+    #         return image_results[0:5]
+        
+    # def threading(self): 
+    #     self.disable_button()
+    
+    #     t1=Thread(target=self.detect_plant) 
+    #     t1.start()        
+        
+    # def disable_button(self):
+    #     self.select_imgs.config(state=DISABLED, text="Loading...")
 
 
 if __name__ == "__main__":
